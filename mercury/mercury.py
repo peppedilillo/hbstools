130b91cc9931161b41a5332d34e1ed6a36614c8f
peppedilillo <peppedilillo@gmail.com>
2024-02-18 16:15:46 +0100

    linter

--- mercury/mercury.py
+++ mercury/mercury.py
@@ -12,8 +12,8 @@ from yaml import dump as write_yaml
 from yaml import safe_load as read_yaml
 from yaml import YAMLError
 
-from hbstools.io import write_ttis_to_fits
 from hbstools import Search
+from hbstools.io import write_ttis_to_fits
 
 LOGO = """
                         
